91d5a53dbc6f9671d81e6cbb4e8a18e21ac0a86b
David Li <lidavid1773@gmail.com>
2023-05-31 21:36:16 -0400

    Product component (currently hardcoding product list with predefined images & json obj data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
