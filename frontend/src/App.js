283a0523ec7228a813a472032ce5bc892556b7c1
David Li <lidavid1773@gmail.com>
2023-06-01 18:03:33 -0400

    Add react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
