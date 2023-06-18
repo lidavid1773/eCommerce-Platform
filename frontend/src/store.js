1f6f2ee7934b31cfe5aceb6588df41f35c319988
David Li <lidavid1773@gmail.com>
2023-06-18 19:11:47 -0400

    Get product details endpoint

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,8 +6,7 @@ const store = configureStore({
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true
+    getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
 export default store;
