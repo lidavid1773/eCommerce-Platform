87531ca4d7a41dd2abe98a8e32cf98566b668564
David Li <lidavid1773@gmail.com>
2023-06-20 21:22:53 -0400

    HomeScreen cleanup for rendered content through guard clauses

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,28 +7,31 @@ import Message from "../components/Message";
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
-  return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
+  const renderContent = () => {
+    if (isLoading) return <Loader />;
+
+    if (error)
+      return (
         <Message variant="danger">
           {error?.data?.message || error.error}
         </Message>
-      ) : (
-        <>
-          <h1>Latest Products</h1>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-        </>
-      )}
-    </>
-  );
+      );
+
+    return (
+      <>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </>
+    );
+  };
+
+  return <>{renderContent()}</>;
 };
 
 export default HomeScreen;
