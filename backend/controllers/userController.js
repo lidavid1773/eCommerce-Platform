0ab713449b21fda15d485ce6e779f155f97eb339
David Li <lidavid1773@gmail.com>
2023-06-23 10:25:05 -0400

    Logout and register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import generateJWT from "../utils/generateJWT.js";
 
 // @route POST /api/users/login
 const loginUser = asyncHandler(async (req, res) => {
@@ -13,17 +13,7 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "30d"
-  });
-
-  // Set JWT as a HTTP-Only cookie
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-  });
+  generateJWT(res, user._id);
 
   res.json({
     _id: user._id,
@@ -35,12 +25,42 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @route POST /api/users
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("Register user");
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  generateJWT(res, user._id);
+
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin
+  });
 });
 
 // @route POST /api/users/logout
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("Logout user + clear cookie");
+  // Clear cookie
+  res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @route GET /api/users/profile
