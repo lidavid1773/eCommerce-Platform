7b3970e8e0fe702abe67c423cfa8ddfd427cfad3
David Li <lidavid1773@gmail.com>
2023-07-03 11:43:40 -0400

    Create & Get Orders slice, screens, methods, ui

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -41,6 +41,10 @@ const cartSlice = createSlice({
     savePaymentMethod: (state, action) => {
       state.paymentMethod = action.payload;
       return updateCart(state);
+    },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return updateCart(state);
     }
   }
 });
@@ -49,7 +53,8 @@ export const {
   addToCart,
   removeFromCart,
   saveShippingAddress,
-  savePaymentMethod
+  savePaymentMethod,
+  clearCartItems
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
