ff39aaa64a01867647cb0f2fbd6ce90c03b3987d
David Li <lidavid1773@gmail.com>
2023-07-10 14:12:02 -0400

    Upload product image

--- frontend/src/screens/admin/ProductEditScreen.js
+++ frontend/src/screens/admin/ProductEditScreen.js
@@ -7,7 +7,8 @@ import FormContainer from "../../components/FormContainer";
 import { toast } from "react-toastify";
 import {
   useUpdateProductMutation,
-  useGetProductDetailsQuery
+  useGetProductDetailsQuery,
+  useUploadProductImageMutation
 } from "../../slices/productsApiSlice";
 
 const ProductEditScreen = () => {
@@ -31,6 +32,9 @@ const ProductEditScreen = () => {
 
   const [updateProduct, { isLoading: loadingUpdate }] =
     useUpdateProductMutation();
+
+  const [uploadProductImage, { isLoading: loadingUpload }] =
+    useUploadProductImageMutation();
 
   useEffect(() => {
     if (product) {
@@ -61,6 +65,18 @@ const ProductEditScreen = () => {
       toast.success("Product updated");
       refetch();
       navigate("/admin/productlist");
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
     } catch (err) {
       toast.error(err?.data?.message || err.error);
     }
@@ -101,7 +117,20 @@ const ProductEditScreen = () => {
               ></Form.Control>
             </Form.Group>
 
-            {/* PLACEHOLDER FOR IMAGE INPUT*/}
+            <Form.Group controlId="image" className="my-2">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter image url"
+                value={image}
+                onChange={(e) => setImage}
+              ></Form.Control>
+              <Form.Control
+                type="file"
+                label="Choose file"
+                onChange={uploadFileHandler}
+              ></Form.Control>
+            </Form.Group>
 
             <Form.Group controlId="brand" className="my-2">
               <Form.Label>Brand</Form.Label>
