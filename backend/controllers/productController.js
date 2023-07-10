b63f0285214ebee38fa3dfc4859780aa4d93aed0
David Li <lidavid1773@gmail.com>
2023-07-10 13:50:32 -0400

    Edit product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,10 +11,54 @@ const getProducts = asyncHandler(async (req, res) => {
 const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
-  if (product) return res.json(product);
+  if (!product) {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
 
-  res.status(404);
-  throw new Error("Resource not found");
+  return res.json(product);
 });
 
-export { getProducts, getProductById };
+// @route POST /api/products
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/image/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description"
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @route PUT /api/products/:id
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+
+  product.name = name;
+  product.price = price;
+  product.description = description;
+  product.image = image;
+  product.brand = brand;
+  product.category = category;
+  product.countInStock = countInStock;
+
+  const updatedProduct = await product.save();
+  res.json(updatedProduct);
+});
+
+export { getProducts, getProductById, createProduct, updateProduct };
